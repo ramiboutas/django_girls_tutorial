3d4fd3152cbee4e38e16c36533cad6215a79f2ea
ramiboutas <ramiboutas@hotmail.com>
2021-05-21 19:44:58 +0200

    media & static folders added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
